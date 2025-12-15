bcd978eee76205007eb5b54e3dad40d8fb72ef11
kaiharvest <indradwiprabowo27@gmail.com>
2025-12-15 14:37:59 +0700

    Sneaky Throws

--- src/test/java/indradwiprabowo/lombok/FileHelperTest.java
+++ src/test/java/indradwiprabowo/lombok/FileHelperTest.java
@@ -1,6 +1,9 @@
 package indradwiprabowo.lombok;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
 
 public class FileHelperTest {
 
@@ -9,6 +12,13 @@ public class FileHelperTest {
         String text = FileHelper.loadFile("pom.xml");
 
         System.out.println(text);
+    }
+
+    @Test
+    void testFileNotFound() throws Exception {
+        Assertions.assertThrows(FileNotFoundException.class, () -> {
+            FileHelper.loadFile("tidakada.xml");
+        });
     }
 
 }
